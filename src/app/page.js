223a4f5a1fa720abbe52bbdf98dd9682207562ca
Jacqueline <jacqueline.leone02@gmail.com>
2024-08-21 16:02:55 -0300

    add: render project elements

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,42 @@
+import Project from "@/components/project";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  const projects = [
+    {
+      title: "C2 Montreal",
+      src: "c2montreal.png",
+      color: "#000000",
+    },
+    {
+      title: "Office Studio",
+      src: "officestudio.png",
+      color: "#8C8C8C",
+    },
+    {
+      title: "Locomotive",
+      src: "locomotive.png",
+      color: "#EFE8D3",
+    },
+    {
+      title: "Silencio",
+      src: "silencio.png",
+      color: "#706D63",
+    },
+  ];
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.body}>
+        {projects.map((project, index) => {
+          return (
+            <Project
+              key={index}
+              title={project.title}
+            />
+          );
+        })}
+      </div>
+    </main>
+  );
 }
